287fcfdcfccb37940f0a1d505189954e2807988e
Jens Binfet <kontakt@jensbinfet.de>
2017-04-21 13:11:39 +0200

    TDF

--- Stones/src/app/product/product.module.ts
+++ Stones/src/app/product/product.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './product/product.component';
 import { BruttoPipe } from './brutto.pipe';
+import { EditProductComponent } from './edit-product/edit-product.component';
+import { MaterialModule } from '@angular/material';
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule, MaterialModule, FormsModule
   ],
-  declarations: [ProductComponent, BruttoPipe],
-  exports: [ProductComponent]
+  declarations: [ProductComponent, BruttoPipe, EditProductComponent],
+  exports: [ProductComponent, EditProductComponent]
 })
 export class ProductModule { }
